ff9236a0f1d9c16ff83e4091023a1f497e09bab9
teddysworkstation <bihelectrons@gmail.com>
2026-04-09 18:52:20 -0700

    Create About Page component for company information

---
+++ src/routes/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to Prime Burchery! We are committed to providing the highest quality products and exceptional customer service. Our team is passionate about what we do and strives to continuously improve the experience for our customers.</p>
+        </div>
+    );
+};
+
+export default AboutPage;
